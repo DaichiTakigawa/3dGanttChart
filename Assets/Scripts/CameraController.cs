e3d38be074485d391f9125f1184d9af35703b8cd
DaichiTakigawa <takigawa@takigawaMacBookPro.local>
2019-12-12 12:01:29 +0900

    なんかよくわからんが勝手にいろんなファイルが改変されていたので一応コミット。

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private float rotate_speed = 1f;
-    [SerializeField]
-    private float mouse_move_speed = 1f;
-    [SerializeField]
-    private float pan_speed = 1f;
-
+    private float rotate_speed = 0.05f;
+    private float move_speed = 0.1f;
+    private float pan_speed = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetAxis("Fire2") == 1f) {
              transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotate_speed, Input.GetAxis("Mouse X") * rotate_speed, 0));
              float x = transform.rotation.eulerAngles.x;
@@ -36,6 +33,36 @@ public class CameraController : MonoBehaviour
         float mouse_scroll = Input.GetAxis("Mouse ScrollWheel");
         if (mouse_scroll != 0f) {
             transform.position += transform.forward * mouse_scroll * pan_speed;
+        }
+        */
+
+        Touch[] touchs = Input.touches;
+        if (Input.touchCount == 1 && Input.touches[0].phase == TouchPhase.Moved) {
+            float touchX = touchs[0].deltaPosition.x;
+            float touchY = touchs[0].deltaPosition.y;
+             transform.Rotate(new Vector3(-touchY * rotate_speed, touchX * rotate_speed, 0));
+             float x = transform.rotation.eulerAngles.x;
+             if (85 <= x && x <= 95) {
+                 x = ((x-90) > 0 ? 95 : 85);
+             }
+             if (265 <= x && x <= 275) {
+                 x = ((x-270) > 0 ? 275 : 265);
+             }
+             float y = transform.rotation.eulerAngles.y;
+             transform.rotation = Quaternion.Euler(x, y, 0);
+        } else if (Input.touchCount == 2) {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrePos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrePos = touchOne.position - touchOne.deltaPosition;
+
+            float preMagnitude = (touchOnePrePos-touchZeroPrePos).magnitude;
+            float currentMagnitude = (touchOne.position-touchZero.position).magnitude;
+
+            float defference = currentMagnitude - preMagnitude;
+
+            transform.position += transform.forward * defference * pan_speed;
         }
 
     }
